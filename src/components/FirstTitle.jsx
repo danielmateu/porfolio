f3f6773ec8fdbebdb120459e3c77fb59841a99f2
Daniel Mateu <danielmateu86@gmail.com>
2023-02-17 13:15:31 +0100

    Estilos en los titulos

--- src/components/FirstTitle.jsx
+++ src/components/FirstTitle.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-scroll'
 
 export const FirstTitle = () => {
     return (
-        <div className="min-h-screen flex flex-col justify-center items-center bg-music bg-fixed bg-cover bg-center bg-no-repeat" id='hero'>
-            <h2 className='text-4xl text-center text-gray-200 bg-gray-600  bg-opacity-40 dark:bg-black dark:bg-opacity-40 transition-all p-96 w-full'>
+        <div className="flex justify-center items-center  min-h-screen bg-music bg-fixed bg-cover bg-center bg-no-repeat" id='hero'>
+            <h2 className='py-96 text-4xl text-center justify-center text-gray-200 bg-gray-600  bg-opacity-40 dark:bg-black dark:bg-opacity-40 transition-all w-full'>
                 Soy el <Link to='dani' className='hover:text-blue-200 transition-colors cursor-pointer' spy={true} smooth={true} offset={0} duration={500} >Frontend Developer</Link>
             </h2>
         </div>
