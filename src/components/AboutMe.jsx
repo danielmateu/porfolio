03b8371983763a643f415eb4dc983211d69cb9b7
Daniel Mateu <danielmateu86@gmail.com>
2023-02-18 22:42:59 +0100

    Cambios en about, etc

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -11,11 +11,15 @@ const Aboutme = () => {
             <p
                 className='text-gray-600 dark:text-gray-400 text-justify '
             >
-                Tengo una experiencia de dos años como programador. Me especializo en el desarrollo de aplicaciones web y móviles, utilizando tecnologías modernas y de vanguardia.<br /><br />
-                En el front end, utilizo ReactJS, NextJS y Tailwind para crear interfaces de usuario interactivas y dinámicas. Con estas herramientas, puedo construir sitios web de alta calidad y aplicaciones móviles que son altamente receptivas y fáciles de usar.<br /><br />
-                En el backend, utilizo NodeJS, Express y MongoDB para crear soluciones escalables y eficientes que permiten a los usuarios interactuar con los datos de su sitio web. Con estas tecnologías, puedo construir aplicaciones robustas y escalables que pueden manejar grandes volúmenes de datos y transacciones complejas.<br /><br />
-                Me enorgullece presentar algunos de los proyectos en los que he trabajado, desde un ecommerce completo hasta un juego de Tic Tac Toe, y muchos más. Estos proyectos demuestran mi capacidad para crear aplicaciones innovadoras y atractivas que satisfacen las necesidades de los usuarios y las empresas por igual.<br /><br />
-                Estoy siempre buscando nuevos desafíos y oportunidades para aprender y mejorar mis habilidades como desarrollador. Si tienes un proyecto interesante que te gustaría discutir, no dudes en <strong><Link className='cursor-pointer' to="contact" spy={true} smooth={true} offset={0} duration={500}>contactarme</Link></strong>. Me encantaría hablar contigo acerca de cómo podemos trabajar juntos para crear algo genial!
+                Tengo una experiencia de 3 años como programador. Me especializo en el desarrollo de <strong>aplicaciones web</strong> y <strong>móviles</strong> , utilizando tecnologías modernas y de vanguardia.<br /><br />
+
+                En el front end, utilizo <strong> ReactJS</strong>, <strong>NextJS</strong> y <strong>Tailwind</strong> para crear interfaces de usuario interactivas y dinámicas. Con estas herramientas, puedo construir sitios web de alta calidad y aplicaciones móviles que son altamente receptivas y fáciles de usar.<br /><br />
+
+                En el backend, utilizo <strong>NodeJS</strong>, <strong>Express</strong> y <strong>MongoDB</strong> para crear soluciones escalables y eficientes que permiten a los usuarios interactuar con los datos de su sitio web. Con estas tecnologías, puedo construir aplicaciones robustas y escalables que pueden manejar grandes volúmenes de datos y transacciones complejas.<br /><br />
+
+                Me enorgullece presentar algunos de los proyectos en los que he trabajado, desde un <strong><a target='_blank' href="https://solidcamper.herokuapp.com/" >ecommerce</a></strong> completo hasta un juego de <strong><a target='_blank' href="https://tic-tac-toe-ecru-two.vercel.app/">Tic Tac Toe</a></strong>, y mucho más. Estos proyectos demuestran mi capacidad para crear aplicaciones innovadoras y atractivas que satisfacen las necesidades de los usuarios y las empresas por igual.<br /><br />
+
+                Estoy siempre buscando nuevos desafíos y oportunidades para aprender y mejorar mis habilidades como desarrollador. Si tienes un proyecto interesante que te gustaría discutir, no dudes en <strong><Link className='cursor-pointer' to="contact" spy={true} smooth={true} offset={0} duration={500}>contactar</Link></strong>. Me encantaría hablar contigo acerca de cómo podemos trabajar juntos para crear algo genial!
             </p>
         </div>
 
