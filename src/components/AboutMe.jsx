c808769799ce3cafaac6c093384d2cb38d1d44db
Daniel Mateu <danielmateu86@gmail.com>
2023-02-19 19:21:37 +0100

    Ajuste de padding en about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,7 +5,7 @@ import { Layout } from './Layout/Layout'
 const Aboutme = () => {
     return (
 
-        <div className="flex flex-col gap-2  px-20 sm:px-52 py-80 sm:py-20 min-h-screen" id='about_me'>
+        <div className="flex flex-col gap-2  px-10 sm:px-52 py-80 sm:py-20 min-h-screen" id='about_me'>
             <h3 className='mb-4 text-4xl text-gray-800 dark:text-gray-400 font-semibold'>Sobre Mi</h3>
 
             <p
