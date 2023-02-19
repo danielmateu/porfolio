57d245ea4a281aaa06b0a7b758a1e06085bbab93
Daniel Mateu <danielmateu86@gmail.com>
2023-02-19 21:42:25 +0100

    Ahora si

--- src/components/SmallNav.jsx
+++ src/components/SmallNav.jsx
@@ -12,20 +12,20 @@ export const SmallNav = () => {
             <div className='flex justify-center items-center py-4'>
                 <div
                     onClick={() => setOpen(!open)}
-                    className="text-gray-800 hover:text-gray-400 hover:rotate-90  text-2xl hover:text-xs absolute right-8 pt-2 transition-all sm:hidden"
+                    className="text-gray-800 dark:text-gray-400 hover:text-gray-400 hover:rotate-90  text-2xl hover:text-base absolute right-8 pt-2 transition-all sm:hidden"
                 >
                     <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
                     {/* Down */}
                 </div>
 
-                <Link to='hero' spy={true} smooth={true} offset={50} duration={500} className={"absolute left-8 cursor-pointer text-gray-800 hover:text-gray-600 dark:hover:text-gray-200 transition-all "} >DM</Link>
+                <Link to='hero' spy={true} smooth={true} offset={50} duration={500} className={"absolute left-8 cursor-pointer text-gray-800  hover:text-gray-600 dark:hover:text-gray-200 transition-all "} >DM</Link>
             </div>
             <ul className={`flex flex-col items-center pt-10 pb-6 bg-gray-100  dark:bg-gray-800 w-full transition-all duration-500 absolute ease-in  gap-4  z-[-1]  ${open ? 'top-0' : 'top-[-40vh]'}`}>
                 {
                     Links.map(link => (
                         <li key={link.id}>
                             <Link to={link.to} spy={true} smooth={true} offset={0} duration={500}
-                                className='cursor-pointer text-gray-800 hover:text-gray-600 dark:hover:text-gray-200 transition-all' >
+                                className='cursor-pointer text-gray-800 hover:text-gray-600 dark:hover:text-gray-200 transition-all dark:text-gray-400' >
                                 {link.data}
                             </Link>
                         </li>
