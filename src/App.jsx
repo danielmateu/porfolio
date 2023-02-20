09f6a93908862abe12d0b1bd86dca4753d951607
Daniel Mateu <danielmateu86@gmail.com>
2023-02-20 11:51:02 +0100

    second chance

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './index.css'
 
 import Home from './components/Home'
+import { Helmet } from 'react-helmet'
 
-function App({}) {
+
+function App({ }) {
   return (
     <>
+      <Helmet>
+        <meta property="og:title" content="Daniel Mateu Porfolio" />
+        <meta property="og:description" content="Porfolio de Daniel Mateu" />
+        <meta property="og:image" content="./src/assets/previsualizacion.webp" />
+      </Helmet>
       <Home />
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Helmet } from 'react-helmet'
+
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <Helmet>
-      <meta property="og:title" content="Daniel Mateu Porfolio" />
-      <meta property="og:description" content="Porfolio de Daniel Mateu" />
-      <meta property="og:image" content="./src/assets/previsualizacion.webp" />
-    </Helmet>
-
+  
     <React.StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </React.StrictMode>
-  </>
+  
 )
