5476f0998daa25323e04edbd23538836ce6e0933
Daniel Mateu <danielmateu86@gmail.com>
2023-02-20 13:51:43 +0100

    Last fucking change... I hope!

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,35 @@ function App({ }) {
 
   //useEffect to create the metaTags
   useEffect(() => {
-    const metaTitle = document.createElement('meta')
-    metaTitle.setAttribute('property', 'og:title')
-    metaTitle.setAttribute('content', 'Daniel Mateu Portfolio')
-    document.head.appendChild(metaTitle)
+    const metaTags = [
+      { name: 'description', content: 'Descripción de tu sitio web' },
+      { property: 'og:title', content: 'Título para redes sociales' },
+      { property: 'og:description', content: 'Descripción para redes sociales' },
+      { property: 'og:image', content: './src/assets/previsualizacion.png' }
+    ];
 
-    const metaDescription = document.createElement('meta')
-    metaDescription.setAttribute('property', 'og:description')
-    metaDescription.setAttribute('content', 'Portfolio de Daniel Mateu')
-    document.head.appendChild(metaDescription)
+    metaTags.forEach(meta => {
+      const element = document.querySelector(`meta[${meta.property ? 'property' : 'name'}="${meta.property || meta.name}"]`);
 
-    const metaImage = document.createElement('meta')
-    metaImage.setAttribute('property', 'og:image')
-    metaImage.setAttribute('content', './src/assets/previsualizacion.png')
-    document.head.appendChild(metaImage)
+      if (element) {
+        element.setAttribute('content', meta.content);
+      }
+
+    });
+    // const metaTitle = document.createElement('meta')
+    // metaTitle.setAttribute('property', 'og:title')
+    // metaTitle.setAttribute('content', 'Daniel Mateu Portfolio')
+    // document.head.appendChild(metaTitle)
+
+    // const metaDescription = document.createElement('meta')
+    // metaDescription.setAttribute('property', 'og:description')
+    // metaDescription.setAttribute('content', 'Portfolio de Daniel Mateu')
+    // document.head.appendChild(metaDescription)
+
+    // const metaImage = document.createElement('meta')
+    // metaImage.setAttribute('property', 'og:image')
+    // metaImage.setAttribute('content', './src/assets/previsualizacion.png')
+    // document.head.appendChild(metaImage)
   },[])
 
   return (
