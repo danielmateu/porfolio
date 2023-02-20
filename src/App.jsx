741f236182001071e28753860a9162b3c352bf4e
Daniel Mateu <danielmateu86@gmail.com>
2023-02-20 13:21:20 +0100

    Let's try again

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import { Helmet } from 'react-helmet'
 function App({ }) {
   return (
     <>
-      <Helmet>
+      {/* <Helmet>
         <meta property="og:title" content="Daniel Mateu Portfolio" />
         <meta property="og:description" content="Portfolio de Daniel Mateu" />
         <meta property="og:image" content="./src/assets/previsualizacion.png" />
-      </Helmet>
+      </Helmet> */}
       <Home />
     </>
   )
